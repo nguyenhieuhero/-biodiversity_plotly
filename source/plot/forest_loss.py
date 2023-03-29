54eee2b63857d80705578fb96d561f76a5426441
nguyenhieuhero <nguyenhieuhero16@gmail.com>
2023-03-30 02:14:09 +0700

    add interactive graph, demo homepage

--- source/plot/forest_loss.py
+++ source/plot/forest_loss.py
@@ -2,11 +2,15 @@ import plotly.express as px
 import geopandas as gpd
 import pandas as pd
 
-world=gpd.read_file("data\countries.geojson")
-data=pd.read_csv(r"data\annual-change-forest-area.csv")
-full=world.merge(data,left_on=["ISO_A3"],right_on=["Code"])
-full=full[full["Year"]==2000]
-print(full)
-fig = px.choropleth(full,locations="ISO_A3",color="Annual net change in forest area",color_continuous_scale="Viridis",color_continuous_midpoint=0,hover_data=['ADMIN'])
-fig.update_layout(title="Deforestation and Forest Loss")
-fig.show()
+
+
+def forest_loss(year=1990):
+    world=gpd.read_file("data\countries.geojson")
+    data=pd.read_csv(r"data\annual-change-forest-area.csv")
+    full=world.merge(data,left_on=["ISO_A3"],right_on=["Code"])
+    full=full[full["Year"]==year]
+    # print(full)
+    fig = px.choropleth(full,locations="ISO_A3",color="Annual net change in forest area",color_continuous_scale="Viridis",color_continuous_midpoint=0,hover_data=['ADMIN'],width=1500, height=700)
+    fig.update_layout(title=f"Deforestation and Forest Loss, {year}")
+    # fig.show()
+    return fig
